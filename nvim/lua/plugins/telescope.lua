4e2711a1358926ac56296b61fce77bfcacb9ba2a
jay-dee7 <me@jsdp.dev>
2023-10-18 12:00:07 +0530

    fix: Re-enable harpoon & add monorepo plugin
    Signed-off-by: jay-dee7 <me@jsdp.dev>

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,179 +1,197 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    dependencies = {
-        { "nvim-lua/plenary.nvim" },
-        { "nvim-lua/popup.nvim" },
-        { "nvim-telescope/telescope-github.nvim" },
-        { "nvim-telescope/telescope-project.nvim" },
-        { "nvim-telescope/telescope-ui-select.nvim" },
-        { "nvim-telescope/telescope-rg.nvim" },
-        { "nvim-telescope/telescope-file-browser.nvim" },
-        { "nvim-telescope/telescope-dap.nvim" },
-        { "nvim-telescope/telescope-live-grep-args.nvim" },
-        { 'nvim-telescope/telescope-fzf-native.nvim',    build = 'make' },
-    },
-    config = function()
-        local builtin = require('telescope.builtin')
-        local lga_actions = require("telescope-live-grep-args.actions")
-        local extensions = require('telescope').extensions
-        local fb_actions = extensions.file_browser.actions
-        local sorters = require("telescope.sorters")
-        local previewers = require("telescope.previewers")
-        local trouble = require("trouble.providers.telescope")
-        local actions = require('telescope.actions')
-        local themes = require('telescope.themes')
-        local monorepo = require('monorepo')
-        local function get_ivy()
-            return themes.get_ivy({
-                previewer = true,
-                layout_config = {
-                    height = 65,
-                    prompt_position = "bottom",
-                    preview_width = 0.65
-                },
-            })
-        end
+	'nvim-telescope/telescope.nvim',
+	dependencies = {
+		{ 'nvim-lua/plenary.nvim' },
+		{ 'nvim-lua/popup.nvim' },
+		{ 'nvim-telescope/telescope-github.nvim' },
+		{ 'nvim-telescope/telescope-project.nvim' },
+		{ 'nvim-telescope/telescope-ui-select.nvim' },
+		{ 'nvim-telescope/telescope-rg.nvim' },
+		{ 'nvim-telescope/telescope-file-browser.nvim' },
+		{ 'nvim-telescope/telescope-dap.nvim' },
+		{ 'nvim-telescope/telescope-live-grep-args.nvim' },
+		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+		{ 'imNel/monorepo.nvim' },
+	},
+	config = function()
+		local builtin = require('telescope.builtin')
+		local lga_actions = require('telescope-live-grep-args.actions')
+		local extensions = require('telescope').extensions
+		local fb_actions = extensions.file_browser.actions
+		local sorters = require('telescope.sorters')
+		local previewers = require('telescope.previewers')
+		local trouble = require('trouble.providers.telescope')
+		local actions = require('telescope.actions')
+		local monorepo = require('monorepo')
 
-        require('telescope').setup {
-            defaults = {
-                mappings = {
-                    i = {
-                        ["<esc>"] = actions.close,
-                        ["<c-t>"] = trouble.open_with_trouble,
-                    },
-                    n = {
-                        ["<c-t>"] = trouble.open_with_trouble,
-                    },
-                },
-                vimgrep_arguments = {
-                    'rg',
-                    '--color=never',
-                    '--no-heading',
-                    '--with-filename',
-                    '--line-number',
-                    '--column',
-                    '--smart-case',
-                    '--no-binary',
-                    '--no-hidden',
-                },
-                prompt_prefix = "➜ ",
-                selection_carett = " ",
-                entry_prefix = " ",
-                initial_mode = "insert",
-                selection_strategy = "row",
-                sorting_strategy = "ascending",
-                layout_strategy = "horizontal",
-                layout_config = {
-                    horizontal = {
-                        mirror = false,
-                        width = 0.90,
-                        height = 0.60,
-                        preview_height = 0.7,
-                        preview_width = 0.6,
-                        prompt_position = "bottom",
-                        preview_cutoff = 120,
-                    },
-                },
-                file_ignore_patterns = { '.idea/*', 'cpu.pprof', '*.prof', '.kvstore', 'vendor', 'node_modules' },
-                winblend = 0,
-                border = {},
-                borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
-                color_devicons = true,
-                use_less = false,
-                set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
-                file_sorter = sorters.get_fzy_sorter,
-                generic_sorter = sorters.get_generic_fuzzy_sorter,
-                file_previewer = previewers.vim_buffer_cat.new,
-                grep_previewer = previewers.vim_buffer_vimgrep.new,
-                qflist_previewer = previewers.vim_buffer_qflist.new,
-                buffer_previewer_maker = previewers.buffer_previewer_maker
-            },
-            extensions = {
-                fzf = {
-                    fuzzy = true,                   -- false will only do exact matching
-                    override_generic_sorter = true, -- override the generic sorter
-                    override_file_sorter = true,    -- override the file sorter
-                    case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
-                },
-                file_browser = {
-                    mappings = {
-                        ["i"] = {
-                            ["<C-R>"] = fb_actions.create,
-                        },
-                        ["n"] = {
-                            ["<C-R>"] = fb_actions.create,
-                        },
-                    },
-                },
-                ["ui-select"] = {
-                    get_ivy()
-                },
-                live_grep_args = {
-                    auto_quoting = true,
-                    i = {
-                        ["<C-k>"] = lga_actions.quote_prompt(),
-                        ["<C-l>g"] = lga_actions.quote_prompt({ postfix = ' --iglob ' }),
-                        ["<C-l>t"] = lga_actions.quote_prompt({ postfix = ' -t' }),
-                    }
-                },
-            }
-        }
-        require('telescope').load_extension('fzf')
-        require("telescope").load_extension("ui-select")
-        require("telescope").load_extension("file_browser")
-        require("telescope").load_extension("live_grep_args")
-        require('telescope').load_extension('gh')
-        require('telescope').load_extension('monorepo')
-        -- require("telescope").load_extension("git_worktree")
+		require('telescope').setup({
+			defaults = {
+				mappings = {
+					i = {
+						['<esc>'] = actions.close,
+						['<c-t>'] = trouble.open_with_trouble,
+					},
+					n = {
+						['<c-t>'] = trouble.open_with_trouble,
+					},
+				},
+				vimgrep_arguments = {
+					'rg',
+					'--color=never',
+					'--no-heading',
+					'--with-filename',
+					'--line-number',
+					'--column',
+					'--smart-case',
+					'--no-binary',
+					'--no-hidden',
+				},
+				prompt_prefix = '➜ ',
+				selection_carett = ' ',
+				entry_prefix = ' ',
+				initial_mode = 'insert',
+				selection_strategy = 'row',
+				sorting_strategy = 'ascending',
+				layout_strategy = 'vertical',
+				layout_config = {
+					vertical = {
+						mirror = false,
+						width = 0.90,
+						height = 0.90,
+						preview_height = 0.7,
+						preview_width = 0.6,
+						-- prompt_position = "bottom",
+						-- preview_cutoff = 120,
+					},
+					horizontal = {
+						mirror = false,
+						width = 0.90,
+						height = 0.60,
+						preview_height = 0.7,
+						preview_width = 0.6,
+						prompt_position = 'bottom',
+						preview_cutoff = 120,
+					},
+				},
+				file_ignore_patterns = { '.idea/*', 'cpu.pprof', '*.prof', '.kvstore', 'vendor', 'node_modules' },
+				winblend = 0,
+				border = {},
+				borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+				color_devicons = true,
+				use_less = false,
+				set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
+				file_sorter = sorters.get_fzy_sorter,
+				generic_sorter = sorters.get_generic_fuzzy_sorter,
+				file_previewer = previewers.vim_buffer_cat.new,
+				grep_previewer = previewers.vim_buffer_vimgrep.new,
+				qflist_previewer = previewers.vim_buffer_qflist.new,
+				buffer_previewer_maker = previewers.buffer_previewer_maker,
+			},
+			extensions = {
+				fzf = {
+					fuzzy = true, -- false will only do exact matching
+					override_generic_sorter = true, -- override the generic sorter
+					override_file_sorter = true, -- override the file sorter
+					case_mode = 'smart_case', -- or "ignore_case" or "respect_case"
+				},
+				file_browser = {
+					mappings = {
+						['i'] = {
+							['<C-R>'] = fb_actions.create,
+						},
+						['n'] = {
+							['<C-R>'] = fb_actions.create,
+						},
+					},
+				},
+				['ui-select'] = {},
+				live_grep_args = {
+					auto_quoting = true,
+					i = {
+						['<C-k>'] = lga_actions.quote_prompt(),
+						['<C-l>g'] = lga_actions.quote_prompt({ postfix = ' --iglob ' }),
+						['<C-l>t'] = lga_actions.quote_prompt({ postfix = ' -t' }),
+					},
+				},
+			},
+		})
 
-        local project_files = function()
-            vim.fn.system('git rev-parse --is-inside-work-tree')
-            if vim.v.shell_error == 0 then
-                builtin.git_files(get_ivy())
-            else
-                builtin.find_files(get_ivy())
-            end
-        end
+		monorepo.setup({
+			autoload_telescope = false,
+		})
 
-        local opts = { noremap = true, silent = true }
+		require('telescope').load_extension('fzf')
+		require('telescope').load_extension('ui-select')
+		require('telescope').load_extension('file_browser')
+		require('telescope').load_extension('live_grep_args')
+		require('telescope').load_extension('gh')
+		require('telescope').load_extension('monorepo')
+		-- require("telescope").load_extension("git_worktree")
 
-        -- Telescope + LSP
-        vim.keymap.set('n', "<leader>fg", function() builtin.lsp_definitions() end, opts)
-        vim.keymap.set('n', "<leader>jm", function() builtin.diagnostics(get_ivy()) end, opts)
-        vim.keymap.set('n', "<leader>ji", function() builtin.lsp_implementations() end, opts)
+		local opts = { noremap = true, silent = true }
 
-        -- Regular Telescope magic
-        vim.keymap.set('n', '<leader>tr', function() extensions.live_grep_args.live_grep_args(get_ivy()) end, opts)
-        vim.keymap.set('n', '<leader>fb', function() builtin.buffers(get_ivy()) end, opts)
-        vim.keymap.set('n', '<leader>fh', function() builtin.help_tags(get_ivy()) end, opts)
-        vim.keymap.set('n', '<C-p>', function() builtin.git_files(get_ivy()) end, opts)
-        vim.keymap.set('n', '<C-f>', function() builtin.find_files(get_ivy()) end, opts)
-        vim.keymap.set('n', '<leader>sd', function() builtin.diagnostics(get_ivy()) end, opts)
-        vim.keymap.set('n', '<leader>ps', function() builtin.grep_string({ search = vim.fn.input("Grep> ") }) end)
-        vim.keymap.set('n', '<leader>sh', function() builtin.search_history(get_ivy()) end,
-            { noremap = true, silent = true })
+		-- Telescope + LSP
+		vim.keymap.set('n', '<leader>fg', function()
+			builtin.lsp_definitions()
+		end, opts)
+		vim.keymap.set('n', '<leader>jm', function()
+			builtin.diagnostics()
+		end, opts)
+		vim.keymap.set('n', '<leader>ji', function()
+			builtin.lsp_implementations()
+		end, opts)
 
-        -- Telescope Extensions
-        vim.keymap.set('n', '<leader>jis', extensions.gh.issues, { noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>jpl', extensions.gh.pull_request, { noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>jgi', extensions.gh.gist, { noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>gar', extensions.gh.run, { noremap = true, silent = true })
+		-- Regular Telescope magic
+		vim.keymap.set('n', '<leader>tr', function()
+			extensions.live_grep_args.live_grep_args()
+		end, opts)
+		vim.keymap.set('n', '<leader>fb', function()
+			builtin.buffers()
+		end, opts)
+		vim.keymap.set('n', '<leader>fh', function()
+			builtin.help_tags()
+		end, opts)
+		vim.keymap.set('n', '<C-p>', function()
+			builtin.git_files()
+		end, opts)
+		vim.keymap.set('n', '<C-f>', function()
+			builtin.find_files()
+		end, opts)
+		vim.keymap.set('n', '<leader>sd', function()
+			builtin.diagnostics()
+		end, opts)
+		vim.keymap.set('n', '<leader>ps', function()
+			builtin.grep_string({ search = vim.fn.input('Grep> ') })
+		end)
+		vim.keymap.set('n', '<leader>sh', function()
+			builtin.search_history()
+		end, { noremap = true, silent = true })
 
-        -- Telescope Git Stuff
-        vim.keymap.set('n', '<leader>jc', function() builtin.git_commits(get_ivy()) end,
-            { noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>jb', function() builtin.git_branches(get_ivy()) end,
-            { noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>js', function() builtin.git_status(get_ivy()) end, { noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>m', function() extensions.monorepo.monorepo(get_ivy()) end,
-            { noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>n', function() monorepo.toggle_project() end, { noremap = true, silent = true })
-        -- vim.api.nvim_create_autocmd("VimEnter", {
-        --   callback = function()
-        --     if vim.fn.argv(0) == "" then
-        --       builtin.find_files(get_ivy())
-        --     end
-        --   end
-        -- })
-    end
+		-- Telescope Extensions
+		vim.keymap.set('n', '<leader>jis', extensions.gh.issues, { noremap = true, silent = true })
+		vim.keymap.set('n', '<leader>jpl', extensions.gh.pull_request, { noremap = true, silent = true })
+		vim.keymap.set('n', '<leader>jgi', extensions.gh.gist, { noremap = true, silent = true })
+		vim.keymap.set('n', '<leader>gar', extensions.gh.run, { noremap = true, silent = true })
+
+		-- Telescope Git Stuff
+		vim.keymap.set('n', '<leader>jc', function()
+			builtin.git_commits()
+		end, { noremap = true, silent = true })
+		vim.keymap.set('n', '<leader>jb', function()
+			builtin.git_branches()
+		end, { noremap = true, silent = true })
+		vim.keymap.set('n', '<leader>js', function()
+			builtin.git_status()
+		end, { noremap = true, silent = true })
+		vim.keymap.set('n', '<leader>mt', function()
+			monorepo.toggle_project()
+		end, { noremap = true, silent = true })
+		-- vim.api.nvim_create_autocmd("VimEnter", {
+		--   callback = function()
+		--     if vim.fn.argv(0) == "" then
+		--       builtin.find_files()
+		--     end
+		--   end
+		-- })
+	end,
 }
--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,58 +1,58 @@
 return {
-    'akinsho/toggleterm.nvim',
-    config = function()
-        local ok, toggleterm = pcall(require, "toggleterm")
-        if not ok then
-            return
-        end
+	'akinsho/toggleterm.nvim',
+	config = function()
+		local ok, toggleterm = pcall(require, 'toggleterm')
+		if not ok then
+			return
+		end
 
-        toggleterm.setup({
-            size = 25,
-            open_mapping = [[<C-\>]],
-            hide_numbers = true,
-            shade_filetypes = {},
-            shade_terminals = true,
-            shading_factor = 2,
-            start_in_insert = true,
-            insert_mappings = true,
-            persist_size = true,
-            direction = "float",
-            close_on_exit = true,
-            shell = vim.o.shell,
-            highlights = {
-                border = 'Normal',
-                background = 'Normal',
-            },
-            float_opts = {
-                border = 'curved',
-                winblend = 5,
-                width = function()
-                    return math.floor(vim.o.columns * 0.90)
-                end,
-                height = function()
-                    return math.floor(vim.o.lines * 0.65)
-                end
-            },
-            winbar = {
-                enabled = true,
-                name_formatter = function(term)
-                    return term.name
-                end
-            }
-        })
+		toggleterm.setup({
+			size = 25,
+			open_mapping = [[<C-\>]],
+			hide_numbers = true,
+			shade_filetypes = {},
+			shade_terminals = true,
+			shading_factor = 2,
+			start_in_insert = true,
+			insert_mappings = true,
+			persist_size = true,
+			direction = 'float',
+			close_on_exit = true,
+			shell = vim.o.shell,
+			highlights = {
+				border = 'Normal',
+				background = 'Normal',
+			},
+			float_opts = {
+				border = 'curved',
+				winblend = 5,
+				width = function()
+					return math.floor(vim.o.columns * 0.80)
+				end,
+				height = function()
+					return math.floor(vim.o.lines * 0.55)
+				end,
+			},
+			winbar = {
+				enabled = true,
+				name_formatter = function(term)
+					return term.name
+				end,
+			},
+		})
 
-        function _G.set_terminal_keymaps()
-            local opts = { buffer = 0 }
-            vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-            vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-            vim.keymap.set('t', '<C-l>', [[<C-l>]], opts)
-            vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-            vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-            vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-            -- vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-        end
+		function _G.set_terminal_keymaps()
+			local opts = { buffer = 0 }
+			vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+			vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+			vim.keymap.set('t', '<C-l>', [[<C-l>]], opts)
+			vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+			vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+			vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+			-- vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+		end
 
-        -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-        vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
-    end
+		-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+		vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+	end,
 }
